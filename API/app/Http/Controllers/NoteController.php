c2b7839fe448eadc4d6398bdacc0ac6f6ae8c3ed
Habibullah Nahid <nahidex@gmail.com>
2015-11-30 21:46:38 +0600

    Initial commit

---
+++ API/app/Http/Controllers/NoteController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace API\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use API\Http\Requests;
+use API\Http\Controllers\Controller;
+use Illuminate\Routing\Route;
+
+use API\Note;
+
+class NoteController extends Controller
+{
+    public function __construct(){
+       $this->middleware('cors');
+       $this->beforeFilter('@find', ['only' => ['show', 'update', 'destroy']]);
+    }
+
+    public function find(Route $route){
+        $this->note = Note::find($route->getParameter('notes'));
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $notes = Note::all();
+        return response()->json($notes->toArray());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Note::create($request->all());
+        return response()->json(['massage' => 'Note Created Successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json($this->note);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $this->note->fill($request->all());
+        $this->note->save();
+        return response()->json(['message' => "Notes Updated"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->note->delete();
+        return response()->json(['message'=>'Note Deleted']);
+    }
+}
---
+++ angular-client/scripts/controllers/note.js
@@ -0,0 +1,44 @@
+'user strict';
+
+angular.module('Client')
+	.controller('IndexNoteCtrl', function ($scope, NoteResource, $location, $timeout) {
+		$scope.Notes = NoteResource.query();
+		$scope.removeNote = function(id){
+			NoteResource.delete({
+				id: id
+			});
+			Materialize.toast("Note Deleted",5000,'red accent-4');
+			$timeout(function(){
+				$location.path('/notes');
+			},1000);
+		};
+
+	})
+	.controller('CreateNoteCtrl', function ($scope, NoteResource, $location, $timeout) {
+		$scope.title = "Create Note";
+		$scope.button = "Create";
+		$scope.Note = {};
+		$scope.saveNote = function () {
+			// console.log($scope.Note);
+			NoteResource.save($scope.Note);
+			Materialize.toast("Note Created",5000,'green accent-4');
+			$timeout(function(){
+				$location.path('/notes');
+			},1000);
+		};
+	})
+	.controller('EditNoteCtrl', function ($scope, NoteResource, $location, $timeout, $routeParams) {
+		$scope.title = "Edit Note";
+		$scope.button = "Update";
+		$scope.Note = NoteResource.get({
+			id: $routeParams.id
+		});
+		$scope.saveNote = function(){
+			NoteResource.update($scope.Note);
+			NoteResource.save($scope.Note);
+			Materialize.toast("Note Updated",5000,'green accent-4');
+			$timeout(function(){
+				$location.path('/notes');
+			},1000);
+		};
+	});
